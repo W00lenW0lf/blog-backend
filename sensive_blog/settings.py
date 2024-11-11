c04284b26762b53add9eab0aab0911d813b1b451
W00lenW0lf <igor45641@gmail.com>
2024-11-11 18:56:27 +0700

    Add index page layout

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -98,10 +98,11 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
 
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
 
-STATIC_ROOT = os.getenv("STATIC_ROOT")
+STATIC_ROOT = os.getenv("static")
 
 MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
 
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+
